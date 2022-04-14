9aba9c4c51fad338f5bad002438be86d19f43c8e
DhrunitSixberries <dhrunit@sixberries.com>
2022-04-14 16:21:57 +0530

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,14 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { sendNewEmail } from './queues/email.queue';
-import { router } from 'bull-board';
+import express from "express";
+import bodyParser from "body-parser";
+import { router } from "bull-board";
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/admin/queues', router);
+app.use("/admin/queues", router);
 
-app.post('/send-email', async (req, res) => {
-    const { message, ...restBody } = req.body;
-    await sendNewEmail({
-        ...restBody,
-        html: `<p>${message}</p>`
-    });
-    res.send({ status: 'ok' });
+app.post("/send-email", async (req, res) => {
+  res.send({ status: "ok" });
 });
 
-app.listen(5000, () => console.log('App running on port 5000'));
+app.listen(5000, () => console.log("App running on port 5000"));
